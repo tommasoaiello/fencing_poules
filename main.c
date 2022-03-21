a20f20f8a88f7e385f750c6ecf99abca73d34dd8
ajeje <aiello.tommaso@gmail.com>
2022-03-21 23:25:54 +0100

    CALCOLO GIRONI

--- main.c
+++ main.c
@@ -7,42 +7,74 @@
 typedef struct list_of_players_s{
     char name[25];
     int ranking;
+    int victories;
+    int scored;
+    int plusminus;
 }list_of_players;
+
+int numberOfPlayers;
+list_of_players* fencers;
+int** poule;
+
+void printList(){
+    int i;
+    printf("Fencers list:\n");
+    for(i=0;i<numberOfPlayers;i++){
+        printf("%s , ", fencers[i].name);
+    }
+    printf("\n");
+}
+
+
+void calcResults(){
+    int i,j;
+    printf("++++++++++++++++++++++++RESULTS+++++++++++++++++++++++\n");
+    for(i=0; i<numberOfPlayers; i++) {
+        printf("%s: ", fencers[i].name);
+        for (j = 0; j < numberOfPlayers; j++) {
+            if (poule[i][j] == 5)
+                fencers[i].victories++;
+            fencers[i].scored= fencers[i].scored + poule[i][j];
+            fencers[i].plusminus= fencers[i].plusminus + poule[j][i];
+        }
+        fencers[i].plusminus= fencers[i].scored - fencers[i].plusminus;
+        printf("VICTORIES %d , SCORED %d , +- %d\n", fencers[i].victories , fencers[i].scored, fencers[i].plusminus);
+    }
+    printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+}
 
 int main(){
     char command[100];
-    int victories=0, scored=0, received=0, plusminus=0;
-    int numberOfPlayers;
     int i,j;
     char sep;
-    list_of_players* fencers;
-    int** poule;
 
+
+    /*GETS THE NUMBER OF FENCERS*/
     if(fgets(command,25,stdin)!=NULL){
         sscanf(command, "%d", &numberOfPlayers);
         fencers= malloc(numberOfPlayers* sizeof(list_of_players));
     }
 
+    /*GETS THE NAME OF THE FENCERS*/
     for(i=0; i<numberOfPlayers; i++){
         if(fgets(command,25,stdin)!=NULL){
             fencers[i].ranking = 0;
+            fencers[i].victories = 0;
+            fencers[i].scored = 0;
+            fencers[i].plusminus = 0;
             for(j=0;j < (strlen(command)-1);j++){
                 fencers[i].name[j] = command[j];
             }
         }
     }
 
-    poule= malloc(numberOfPlayers*sizeof(int*));
+    /*INITIALIZES THE POULE*/
+    poule= malloc((numberOfPlayers)*sizeof(int*));
     for(i=0;i<numberOfPlayers;i++){
-        poule[i]= malloc(numberOfPlayers* sizeof(int));
+        poule[i]= malloc((numberOfPlayers)* sizeof(int));
     }
 
-    for(i=0;i<numberOfPlayers;i++){
-        for(j=0; j<numberOfPlayers;j++){
-            poule[i][j]= i+j;
-        }
-    }
-
+    /*GETS THE RESULT OF THE POULE*/
     while(!feof(stdin)){
         for(i=0;i<numberOfPlayers;i++){
             for(j=0; j<numberOfPlayers;j++){
@@ -54,19 +86,10 @@ int main(){
 
 
     printf("Fencers attending the competition: %d\n", numberOfPlayers);
-    printf("Fencers list:\n");
-    for(i=0;i<numberOfPlayers;i++){
-        printf("%s\n", fencers[i].name);
-    }
 
-    for(i=0;i<numberOfPlayers;i++){
-        printf("%s: ", fencers[i].name);
-        for(j=0; j<numberOfPlayers;j++){
-            printf("%d ", poule[i][j]);
-        }
-        printf("\n");
-    }
+    printList();
 
+    calcResults();
 
     return 0;
 }
