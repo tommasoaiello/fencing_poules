cbb6b8d16e6b52ced5f1578c6c25a399dbc2cd42
tommasoaiello <79937671+tommasoaiello@users.noreply.github.com>
2022-03-26 12:32:40 +0100

    Update main.c
    RISULTATO GIRONE

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct classifica_s{
+    int indice;
+    struct classifica_s *left, *right;
+}classifica;
+
+
 typedef struct list_of_players_s{
     char name[25];
     int ranking;
@@ -15,6 +21,7 @@ typedef struct list_of_players_s{
 int numberOfPlayers;
 list_of_players* fencers;
 int** poule;
+classifica* radice;
 
 void printList(){
     int i;
@@ -25,12 +32,58 @@ void printList(){
     printf("\n");
 }
 
+int compare(int index1, int index2){
+    list_of_players fencer1,fencer2;
+    fencer1= fencers[index1];
+    fencer2= fencers[index2];
+    if(fencer1.victories > fencer2.victories)
+        return 1;
+    else if(fencer1.victories == fencer2.victories && fencer1.plusminus > fencer2.plusminus)
+        return 1;
+    else if(fencer1.plusminus == fencer2.plusminus && fencer1.scored >= fencer2.scored)
+        return 1;
+    else
+        return 0;
+}
+
+
+
+
+classifica* newFencer(int indice){
+    classifica* temp= (classifica*) malloc(sizeof(classifica));
+    temp->indice=indice;
+    temp->left= temp->right= NULL;
+    return temp;
+}
+
+classifica* insertNewFencer(classifica* root, int indice){
+    if(root==NULL)
+        return newFencer(indice);
+    if(compare(indice,root->indice))
+        root->left = insertNewFencer(root->left,indice);
+    else if(compare(root->indice,indice))
+        root->right = insertNewFencer(root->right,indice);
+
+    return root;
+}
+
+void print_fencer(int i){
+    printf("%s: VICTORIES %d , SCORED %d , +- %d\n",fencers[i].name, fencers[i].victories , fencers[i].scored, fencers[i].plusminus);
+}
+
+void in_order_walk(classifica* root){
+    if(root!= NULL){
+        in_order_walk(root->left);
+        print_fencer(root->indice);
+        in_order_walk(root->right);
+    }
+    return;
+}
 
 void calcResults(){
     int i,j;
     printf("++++++++++++++++++++++++RESULTS+++++++++++++++++++++++\n");
     for(i=0; i<numberOfPlayers; i++) {
-        printf("%s: ", fencers[i].name);
         for (j = 0; j < numberOfPlayers; j++) {
             if (poule[i][j] == 5)
                 fencers[i].victories++;
@@ -38,8 +91,11 @@ void calcResults(){
             fencers[i].plusminus= fencers[i].plusminus + poule[j][i];
         }
         fencers[i].plusminus= fencers[i].scored - fencers[i].plusminus;
-        printf("VICTORIES %d , SCORED %d , +- %d\n", fencers[i].victories , fencers[i].scored, fencers[i].plusminus);
     }
+    for(i=0; i< numberOfPlayers; i++){
+        radice = insertNewFencer(radice,i);
+    }
+    in_order_walk(radice);
     printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 }
 
@@ -90,6 +146,7 @@ int main(){
     printList();
 
     calcResults();
+
 
     return 0;
 }
